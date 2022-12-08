e76959bbc2e11cc9da2014494122fa1b3beadb80
GK9621 <gidkwizera1@gmail.com>
2022-12-08 11:17:19 -0800

    print text

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,16 @@
-#include <stdio.h>                                                                                                                                     
+#include <stdio.h>
 
-/**
+/**                                                                                                                                                    
 *main - Entry point
 *
-*Return: Always 0 (success)
-*/
+*Return: Always 0 (success) 
+*/        
 
-int main(void)
-{
-	pirntf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
-}     
+int main(void)                                                                                                                                         
+
+{                                                                                                                                                      
+	        printf("with proper grammar, but the outcome is a piece of art\n");
+                return (0);
+}		
+ 
+
