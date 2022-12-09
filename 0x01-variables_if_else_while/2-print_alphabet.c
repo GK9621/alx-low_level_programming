7259b2fc2fbdaedf911bd5ce52b1084a250235d2
GK9621 <gidkwizera1@gmail.com>
2022-12-09 11:10:08 -0800

    returning alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * main - alphabet return
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int ch = 'a';
+	
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
+
