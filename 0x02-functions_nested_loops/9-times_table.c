4a2113c7a0d73316bcdcba4751e40df90b4c913f
GK9621 <gidkwizera1@gmail.com>
2022-12-13 12:30:30 -0800

    9x9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,7 +16,6 @@ void times_table(void)
 			if ((c / 10) == 0)
 			{
 				if (b == 0)
-				
 					_putchar('0');
 				}
 				if (b != 0)
@@ -26,21 +25,20 @@ void times_table(void)
 				}
 				if (b < 9)
 				{
-					_putchar(',' );
+					_putchar(',');
 					_putchar(' ');
 				}
-			
 			else
 			{
 				_putchar((c / 10) + '0');
 				_putchar((c % 10) + '0');
 				if (b < 9)
 				{
-					_putchar(',' );
+					_putchar(',');
 					_putchar(' ');
 				}
 			}
 			_putchar('\n');
 		}
 	}
-}
+}i
