9c0cd8c6040d58f13cf69c2597156db853c47a71
GK9621 <gidkwizera1@gmail.com>
2022-12-19 08:48:57 -0800

    copy string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,11 +12,11 @@ char *_strcpy(char *dest, char *src)
 	char *start = dest;
 
 	while (*src != '\0')
-		{
-			*dest = *src;
-			dest++;
-			src++;
-		}
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
 	*dest = '\0';
 	return (start);
 }
