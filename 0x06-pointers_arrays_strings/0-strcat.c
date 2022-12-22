3dabb7c336ebc3c29e81034c4f4cbbf05d534509
GK9621 <gidkwizera1@gmail.com>
2022-12-22 08:00:38 -0800

    concatenate

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,15 +9,22 @@
 
 char *_strcat(char *dest, char *src);
 {
-	int a = -1, i;
+	int i;
+	int j;
 
-	for (i = 0; dest[i] != '\0'; i++)
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
 
-		do {
-			a++;
-			dest[i] = src[a];
-			i++;
-		}
-	while (src[a] != '\0');
+	dest[i] = '\0';
 	return (dest);
 }
