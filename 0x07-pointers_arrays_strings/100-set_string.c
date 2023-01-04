0653621d13c10d7daae63fa147787c21da9bab1a
GK9621 <gidkwizera1@gmail.com>
2023-01-04 10:46:51 -0800

    set strng

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * set_string - set values of a to char
+ * @s: pointer
+ * to: pointer to char
+ *
+ * Return: void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
