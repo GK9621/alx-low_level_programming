39e9d7d4d0c9c954c360ff8aa7ac6430df8b470b
GK9621 <gidkwizera1@gmail.com>
2023-01-09 04:43:44 -0800

    whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stido.h>
+
+/**
+ * main - write program that prints name
+ * Renaming program will print new name
+ * path before name should be removed
+ *
+ * @argc: count argument
+ * @argv: vector argument
+ *
+ * Return: return to 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	(void)argc;
+	return(0);
+}
